898ee914e762e722b33ff918062dbe880ec092f8
Nicky <nqinnqin23@gmail.com>
2022-04-23 17:45:41 -0500

    Level 6: OAuth with Google && Secrets functionality (different from guide, secrets are in a separate db and not linked to account)

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 //jshint esversion:6
 
+//add FACEBOOK OAuth
+
 require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
@@ -9,6 +11,8 @@ const mongoose = require("mongoose");
 const session = require("express-session");
 const passport = require("passport");
 const passportLocalMongoose = require("passport-local-mongoose");
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const findOrCreate = require("mongoose-findorcreate");
 
 const app = express();
 
@@ -37,23 +41,69 @@ async function main() {
 
 const userSchema = new mongoose.Schema({
   email: String,
-  password: String
+  password: String,
+  googleId: String
 });
 
 //Strategy that hash and salt PWs, save users to mongodb database
 userSchema.plugin(passportLocalMongoose);
+userSchema.plugin(findOrCreate);
 
 const User = mongoose.model("User", userSchema);
 //end mongoose
 
 passport.use(User.createStrategy());
 
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
+//change serialize/deserialize based off passport code to work for all strategies
+passport.serializeUser(function(user, done) {
+  done(null, user.id);
+});
+passport.deserializeUser(function(id, done) {
+  User.findById(id, function(err, user) {
+    done(err, user);
+  });
+});
+
+passport.use(new GoogleStrategy({
+    clientID: process.env.GOOGLE_CLIENT_ID,
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    callbackURL: "http://localhost:3000/auth/google/secrets"
+    //userProfileURL?
+  },
+  function(accessToken, refreshToken, profile, cb) {
+    console.log(profile);
+    User.findOrCreate({ googleId: profile.id }, function (err, user) {
+      return cb(err, user);
+    });
+  }
+));
+
+
+const secretSchema = new mongoose.Schema({
+  secret: String
+});
+
+const Secret = mongoose.model("Secret", secretSchema);
+
 
 app.get("/", (req, res) => {
   res.render("home");
 });
+
+//google button redirects to this
+app.get('/auth/google',
+  passport.authenticate('google', { scope: ['profile'] }));
+  //profile means email and userid
+
+//redirect page after logged in with google (local auth)
+app.get('/auth/google/secrets',
+//authenticate locally, start session and create cookies
+    passport.authenticate('google', { failureRedirect: '/login' }),
+    function(req, res) {
+
+      // Successful authentication, redirect secrets.
+      res.redirect('/secrets');
+    });
 
 app.get("/register", (req, res) => {
   res.render("register");
@@ -98,11 +148,41 @@ app.post("/login", (req, res) => {
 
 app.get("/secrets", (req, res) => {
   if (req.isAuthenticated()){
-    res.render("secrets");
+
+    Secret.count().exec(function(err, count) {
+      const random = Math.floor(Math.random() * count);
+      Secret.findOne().skip(random).exec(
+        function(err, result) {
+          res.render("secrets", {
+            randomSecret: result.secret
+          });
+        }
+      );
+    });
   } else {
     res.redirect("/login");
   }
 });
+
+app.get("/submit", (req, res) => {
+  if (req.isAuthenticated()){
+    res.render("submit");
+  } else {
+    res.redirect("/login");
+  }
+});
+
+app.post("/submit", (req, res) => {
+  newSecret = new Secret({
+    secret: req.body.secret
+  });
+  newSecret.save( (err) => {
+    if (!err) {
+      console.log("Secret saved!");
+      res.redirect("/secrets");
+    }
+  });
+})
 
 app.get("/logout", (req, res) => {
   req.logout();
